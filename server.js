7ec6ff6843097cc39fabbab7f4f2ad89acb6537d
mymcworld3-droid <mymcworld3@gmail.com>
2026-03-26 19:27:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ io.on('connection', (socket) => {
 
             // 呼叫 Gemini 模型
             const response = await ai.models.generateContent({
-                model: 'gemini-2.5-lite',
+                model: 'gemini-2.5-flash',
                 contents: prompt,
                 config: {
                     responseMimeType: "application/json",
